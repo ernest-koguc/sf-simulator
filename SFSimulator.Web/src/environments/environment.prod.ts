f0cbd542e8941f372e6ce44614f83167e16a11b2
Ernest Koguc <ernestkoguc@gmail.com>
2023-10-19 21:45:49 +0200

    Updated patchnotes and version

--- SFSimulator.Web/src/environments/environment.prod.ts
+++ SFSimulator.Web/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://sfsimulator.xyz',
   sftoolsLogin: 'https://sftools.mar21.eu/request?scope=default+pets+items&origin=SF%20Simulator&redirect=https://sfsimulator.xyz/api/loadFromEndpoint',
-  currentVersion: '1.3'
+  currentVersion: '1.4'
 };
