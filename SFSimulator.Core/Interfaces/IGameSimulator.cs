e1d82faa26aff3b3f8a138c2157d12285ac14544
Ernest Koguc <ernestkoguc@gmail.com>
2024-04-22 20:32:04 +0200

    Reverted IGameSimulator to non-task based

--- SFSimulator.Core/Interfaces/IGameSimulator.cs
+++ SFSimulator.Core/Interfaces/IGameSimulator.cs
@@ -2,6 +2,6 @@
 {
     public interface IGameSimulator
     {
-        Task<SimulationResult> Run(int until, Character character, SimulationOptions simulationOptions, SimulationType simulationType);
+        SimulationResult Run(int until, Character character, SimulationOptions simulationOptions, SimulationType simulationType);
     }
 }
