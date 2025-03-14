990a4b83c656dd64bed719e493c37a25ebf717c9
Ernest Koguc <ernestkoguc@gmail.com>
2025-03-14 23:33:07 +0100

    Removed unused BrowserSerrvice functionality

--- SFSimulator.Frontend/Shared/SharedUtilitity.cs
+++
@@ -1,8 +0,0 @@
-﻿using Radzen;
-
-namespace SFSimulator.Frontend;
-
-public static class SharedUtilitity
-{
-    public static DialogOptions DefaultDialogOptions => new() { CloseDialogOnEsc = true, CloseDialogOnOverlayClick = true };
-}
